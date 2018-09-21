50a4ab9fa402d9ed6cdf76066c2624e777459d4b
autodocs <autodocs>
2018-09-21 17:09:07 +0000

    build based on 48f891c

--- latest/search_index.js
+++ latest/search_index.js
@@ -333,7 +333,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Utility Types",
     "title": "Terminal State",
     "category": "section",
-    "text": "TerminalState and its singleton instance terminalstate are available to use for a terminal state in concert with another state type. It has the appropriate type promotion logic to make its use with other types friendly, similar to nothing and missing.<aside class=\"notice\"> NOTE: This is NOT a replacement for the standard POMDPs.jl isterminal function, though isterminal is implemented for the type. It is merely a convenient type to use for terminal states. </aside><aside class=\"warning\"> WARNING: Early tests suggest that the Julia 1.0 compiler will not be able to efficiently implement union splitting in cases as complex as POMDPs, so using a Union for the state type of a problem can currently have a large overhead. </aside>TerminalState\nterminalstate"
+    "text": "TerminalState and its singleton instance terminalstate are available to use for a terminal state in concert with another state type. It has the appropriate type promotion logic to make its use with other types friendly, similar to nothing and missing.note: Note\nNOTE: This is NOT a replacement for the standard POMDPs.jl isterminal function, though isterminal is implemented for the type. It is merely a convenient type to use for terminal states.warning: Warning\nWARNING: Early tests suggest that the Julia 1.0 compiler will not be able to efficiently implement union splitting in cases as  complex as POMDPs, so using a Union for the state type of a problem can currently have a large overhead.TerminalState\nterminalstate"
 },
 
 {
